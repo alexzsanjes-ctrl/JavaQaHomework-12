61f020bde1b42254078087cd7cf8809ba586f51a
Александр Саломанин <alexzsanjes@gmail.com>
2026-02-02 22:23:11 +0700

    solution corrected

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TodosTest {
+
     @Test
     public void shouldAddThreeTasksOfDifferentType() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
@@ -30,41 +31,12 @@ class TodosTest {
     }
 
     @Test
-    public void shouldSearchMeetingTasksInProjectByQuery() {
-
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
+    public void shouldSearchWhenTaskListIsEmpty() {
 
         Todos todos = new Todos();
 
-        todos.add(meeting);
-
-        Task[] expected = {meeting};
-        Task[] actual = todos.search("Выкатка");
-
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldSearchMeetingTasksInTopicByQuery() {
-
-        Meeting meeting = new Meeting(
-                555,
-                "Выкатка 3й версии приложения",
-                "Приложение НетоБанка",
-                "Во вторник после обеда"
-        );
-
-        Todos todos = new Todos();
-
-        todos.add(meeting);
-
-        Task[] expected = {meeting};
-        Task[] actual = todos.search("НетоБанк");
+        Task[] expected = {};
+        Task[] actual = todos.search("Бином");
 
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -72,37 +44,59 @@ class TodosTest {
     @Test
     public void shouldSearchSimpleTaskByQuery() {
 
-
-        SimpleTask simpleTask1 = new SimpleTask(1, "Открыть счёт в банке");
-        SimpleTask simpleTask2 = new SimpleTask(2, "Откликнуться на вакансию");
-        SimpleTask simpleTask3 = new SimpleTask(3, "Записать видеовизитку");
+        SimpleTask simpleTask = new SimpleTask(1, "Открыть счёт в банке");
 
         Todos todos = new Todos();
 
-        todos.add(simpleTask1);
-        todos.add(simpleTask2);
-        todos.add(simpleTask3);
+        todos.add(simpleTask);
 
-        Task[] expected = {simpleTask1};
+        Task[] expected = {simpleTask};
         Task[] actual = todos.search("Открыть счёт");
 
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void shouldSearchEpicTaskByQuery() {
+    public void shouldSearchDifferentTypesOfTuskByQuery() {
+        SimpleTask simpleTask = new SimpleTask(5, "Открыть счёт в банке");
 
         String[] subtasks = {"Провести тесты", "Найти баги", "Составить баг-репорты"};
         Epic epic = new Epic(4, subtasks);
 
+        Meeting meeting1 = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Meeting meeting2 = new Meeting(
+                556,
+                "Производственная планёрка",
+                "Повышение производительности",
+                "В понедельник утром"
+        );
+
         Todos todos = new Todos();
 
+        todos.add(simpleTask);
         todos.add(epic);
+        todos.add(meeting1);
+        todos.add(meeting2);
 
-        Task[] expected = {epic};
-        Task[] actual = todos.search("баг-репорты");
+        Task[] expected1 = {simpleTask};
+        Task[] expected2 = {epic};
+        Task[] expected3 = {meeting1};
+        Task[] expected4 = {meeting2};
 
-        Assertions.assertArrayEquals(expected, actual);
+        Task[] actual1 = todos.search("Открыть счёт");
+        Task[] actual2 = todos.search("Найти баги");
+        Task[] actual3 = todos.search("НетоБанк");
+        Task[] actual4 = todos.search("планёрка");
+
+        Assertions.assertArrayEquals(expected1, actual1);
+        Assertions.assertArrayEquals(expected2, actual2);
+        Assertions.assertArrayEquals(expected3, actual3);
+        Assertions.assertArrayEquals(expected4, actual4);
     }
-
 }
