c1f2d0b557b5dd29aeadc9ae8049f003b6a0c9ec
Александр Саломанин <alexzsanjes@gmail.com>
2026-02-03 21:00:42 +0700

    test modified

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -57,11 +57,16 @@ class TodosTest {
     }
 
     @Test
-    public void shouldSearchDifferentTypesOfTuskByQuery() {
-        SimpleTask simpleTask = new SimpleTask(5, "Открыть счёт в банке");
+    public void shouldSearchMultipleTuskByQuery() {
+        SimpleTask simpleTask1 = new SimpleTask(5, "Установить приложение Нетология");
+        SimpleTask simpleTask2 = new SimpleTask(5, "Установить приложение Тестировщик");
 
-        String[] subtasks = {"Провести тесты", "Найти баги", "Составить баг-репорты"};
-        Epic epic = new Epic(4, subtasks);
+
+        String[] subtasks1 = {"Провести тесты на производительность", "Найти баги", "Составить баг-репорты"};
+        String[] subtasks2 = {"Сделать ТО", "Проверить оборудование на производительность", "Составить отчет"};
+
+        Epic epic1 = new Epic(4, subtasks1);
+        Epic epic2 = new Epic(6, subtasks2);
 
         Meeting meeting1 = new Meeting(
                 555,
@@ -71,32 +76,33 @@ class TodosTest {
         );
 
         Meeting meeting2 = new Meeting(
-                556,
-                "Производственная планёрка",
-                "Повышение производительности",
-                "В понедельник утром"
+                555,
+                "Тестирование приложения",
+                "Приложение НетоБанка",
+                "В среду утром"
         );
 
         Todos todos = new Todos();
 
-        todos.add(simpleTask);
-        todos.add(epic);
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(epic1);
+        todos.add(epic2);
         todos.add(meeting1);
         todos.add(meeting2);
 
-        Task[] expected1 = {simpleTask};
-        Task[] expected2 = {epic};
-        Task[] expected3 = {meeting1};
-        Task[] expected4 = {meeting2};
+        Task[] expected1 = {simpleTask1, simpleTask2};
+        Task[] expected2 = {epic1, epic2};
+        Task[] expected3 = {meeting1,meeting2};
 
-        Task[] actual1 = todos.search("Открыть счёт");
-        Task[] actual2 = todos.search("Найти баги");
+        Task[] actual1 = todos.search("Установить");
+        Task[] actual2 = todos.search("производительность");
         Task[] actual3 = todos.search("НетоБанк");
-        Task[] actual4 = todos.search("планёрка");
+        Task[] actual4 = todos.search("приложения");
 
         Assertions.assertArrayEquals(expected1, actual1);
         Assertions.assertArrayEquals(expected2, actual2);
         Assertions.assertArrayEquals(expected3, actual3);
-        Assertions.assertArrayEquals(expected4, actual4);
+        Assertions.assertArrayEquals(expected3, actual4);
     }
 }
